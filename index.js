7c0f670c9776b0baeaac5e551e4cad3bddbefb25
mhadikazemian <mhadi.kazemian@gmail.com>
2022-05-10 16:16:27 +0430

    Add mongoose to the project.

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 const genres = require('./routes/genres');
+
+mongoose.connect('mongodb://localhost/vidly')
+    .then(() => console.log('connected successfully'))
+    .catch(err => console.err('Could not connect to db'));
 
 app.use(express.json());
 app.use('/api/genres', genres);
